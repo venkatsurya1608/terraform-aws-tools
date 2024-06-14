3fb9c35b841f51c5763cb3cd717240b179ac8317
venkatsurya <venkatsurya1608@gmail.com>
2024-06-14 12:35:49 +0530

    pipeline

--- CICD/jenkins-agent.sh
+++ CICD/jenkins-agent.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo yum install fontconfig java-17-openjdk
+sudo yum install fontconfig java-17-openjdk -y
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
