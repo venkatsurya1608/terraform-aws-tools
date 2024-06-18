6f53f17ae51cbe1c87752886dd0f6af0532af774
venkatsurya <venkatsurya1608@gmail.com>
2024-06-18 15:52:01 +0530

    infra

--- CICD/data.tf
+++ CICD/data.tf
@@ -20,3 +20,25 @@ data "aws_ami" "ami_info" {
     }
 }
 
+
+
+data "aws_ami" "nexus_ami_info" {
+
+    most_recent = true
+    owners = ["679593333241"]
+
+    filter {
+        name   = "name"
+        values = ["SolveDevOps-Nexus-Server-Ubuntu20.04-20240511-*"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
--- CICD/main.tf
+++ CICD/main.tf
@@ -34,6 +34,33 @@ module "jenkins_agent" {
   )
 }
 
+resource "aws_key_pair" "nexus" {
+  key_name   = "nexus"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGB/HuwOlK9vKk0LInlSdbvrG0HSlCjgCDJIenarzPAZ h9010@Harish"
+}
+
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "nexus"
+
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = ["sg-0901b3bc038adbb53"]
+  # convert StringList to list and get first element
+  subnet_id = "subnet-02eef0a1b1c30ffa4"
+  ami = data.aws_ami.nexus_ami_info.id
+  key_name = aws_key_pair.nexus.key_name
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
+  tags = {
+    Name = "nexus"
+  }
+}
+
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
@@ -59,16 +86,16 @@ module "records" {
       ]
       allow_overwrite = true
     },
-    # {
-    #   name    = "nexus"
-    #   type    = "A"
-    #   ttl     = 1
-    #   allow_overwrite = true
-    #   records = [
-    #     module.nexus.private_ip
-    #   ]
-    #   allow_overwrite = true
-    # }
+    {
+      name    = "nexus"
+      type    = "A"
+      ttl     = 1
+      allow_overwrite = true
+      records = [
+        module.nexus.private_ip
+      ]
+      allow_overwrite = true
+    }
   ]
 
 }
